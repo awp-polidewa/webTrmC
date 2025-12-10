70ad5f1b4a34809f8ac298f1c4e8ea2ab20ffcaa
Afril <upgradeskills223@gmail.com>
2025-12-10 09:19:32 +0800

    update fitur togle

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ togleMode.addEventListener("click", () => {
   togleMode.classList.toggle("active");
   document.body.classList.toggle("dark");
 });
+
+const toggleMenu = document.querySelector(".togle-menu");
+const ul = document.querySelector("nav ul");
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("active");
+  ul.classList.toggle("slide");
+  togleMode.classList.toggle("ubah");
+});
